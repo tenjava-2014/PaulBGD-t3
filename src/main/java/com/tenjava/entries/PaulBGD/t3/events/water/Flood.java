db51260f2be82f4b5e106234b28207ffa95844a3
Paul Sauve <paul@paulbgd.me>
2014-07-12 17:15:42 -0500

    Gave up on wave

--- src/main/java/com/tenjava/entries/PaulBGD/t3/events/water/Flood.java
+++ src/main/java/com/tenjava/entries/PaulBGD/t3/events/water/Flood.java
@@ -1,18 +1,16 @@
 package com.tenjava.entries.PaulBGD.t3.events.water;
 
-import com.tenjava.entries.PaulBGD.t3.TenJava;
 import com.tenjava.entries.PaulBGD.t3.events.NaturalEvent;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class Flood extends NaturalEvent {
 
     private final BlockFace[] faces = new BlockFace[]{BlockFace.NORTH, BlockFace.WEST, BlockFace.EAST, BlockFace.WEST};
 
     public Flood() {
-        super("Flood", 3);
+        super("Flood", 10);
 
         NaturalEvent.getEvents().add(this);
     }
@@ -31,21 +29,6 @@ public class Flood extends NaturalEvent {
             return;
         }
         block.getRelative(BlockFace.UP).setType(Material.WATER);
-        wave(block.getRelative(BlockFace.UP));
     }
 
-    private void wave(Block block) {
-        for(BlockFace face : faces) {
-            final Block relative = block.getRelative(face);
-            if(relative.getType() == Material.AIR && TenJava.getRandom().nextInt(50) != 0) {
-                relative.setType(Material.WATER);
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        wave(relative);
-                    }
-                }.runTaskLater(TenJava.getPlugin(), 15);
-            }
-        }
-    }
 }
